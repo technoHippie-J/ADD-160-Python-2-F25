b66a6f6d8fdda8d40b3d5ca3baed84507bb502ed
The-Cognitive-Architect <tjanus@students.mchenry.edu>
2025-09-07 22:16:26 -0500

    pep assignment

--- unit1_oop/wk02/pep/__main__.py
+++ unit1_oop/wk02/pep/__main__.py
@@ -39,19 +39,49 @@ Follow these step-by-step directions to improve the code:
 7. Hand in your updated code by uploading it to GitHub and submitting the link.
 """
 
+# Test Values
+value_a = 15
+value_b = 10
 
-def fx(a, b):
-    if a > 0 and b > 0:
-        if a > b:
-            for i in range(b):
-                print(a)
-        else:
-            for j in range(a):
-                print(b)
-    elif a == 0 or b == 0:
+
+def repeat_or_nonpos(value_a, value_b):
+    """
+    Repeating Value or Absolute Value Function
+    - Takes two values, checks:
+        - is int?
+            - no->raise
+        - is 0?
+            -kill
+        - both over 0?
+            -get bigger and smaller
+                -print bigger(smaller*)
+        - otherwise, return non-positive difference (smaller-larger)
+    """
+    if not isinstance(value_a, int) or not isinstance(value_b, int):
+        raise TypeError("Both inputs must be integers.")
+
+    # Check for zero, kill if present
+    if value_a == 0 or value_b == 0:
         return "Zero found"
-    else:
-        if a < b:
-            return a - b
-        else:
-            return b - a
+
+    # Check if both values above 0, set larger and smaller,
+    # print the larger number x times = to smaller
+    if value_a > 0 and value_b > 0:
+        larger = max(value_a, value_b)
+        smaller = min(value_a, value_b)
+
+        for i in range(smaller):
+            print(larger)
+
+        return None
+
+    # Returns non-positive difference between the two values if one is negative
+    return -abs(value_a - value_b)
+
+
+def main():
+    repeat_or_nonpos(value_a, value_b)
+
+
+if __name__ == "__main__":
+    main()
